cb8982fbd98dc7b1a18fdb8e4ae997bbe02e6631
caitsith2 <d_good@caitsith2.com>
2019-04-14 22:39:24 -0700

    Undo a line that wasn't supposed to be in main commit.

--- src/main/java/sg4e/maikatracker/KeyItemPanel.java
+++ src/main/java/sg4e/maikatracker/KeyItemPanel.java
@@ -98,8 +98,7 @@ public class KeyItemPanel extends JPanel {
         add(itemImage, BorderLayout.CENTER);
         
         locationLabel = new JLabel(UNKNOWN_LOCATION, JLabel.CENTER);
-        locationLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-        locationLabel.setForeground(new Color(255,255,255));
+        locationLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);        
         add(locationLabel, BorderLayout.CENTER);
     }
     
